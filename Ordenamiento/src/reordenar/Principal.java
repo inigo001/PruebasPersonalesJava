05e89cacf762110072299ebd78008a64e97c7584
inigo001 <ivique001@gmail.com>
2017-05-25 23:17:18 +0200

    first commit

---
+++ Ordenamiento/src/reordenar/Principal.java
@@ -0,0 +1,41 @@
+package reordenar;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numArray = createArray(16, 1000);
+		
+		showArrayContent(numArray);
+
+		int[] sortedArray = Sort.quickSort(numArray);
+		
+		showArrayContent(sortedArray);
+		
+	}
+
+	private static int[] createArray(int amount, int maximum) {
+
+		int[] numArray = new int[amount];
+
+		for (int i = 0; i < numArray.length; i++) {
+			int newNumber = (int) (Math.random() * maximum);
+			numArray[i] = newNumber;
+		}
+
+		return numArray;
+	}
+
+	private static void showArrayContent(int[] numArray) {
+
+		System.out.println("-----\nArray Length: " + numArray.length);
+		System.out.println("Array Content:");
+
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.print(numArray[i] + ", ");
+		}
+
+	}
+
+}
---
+++ Ordenamiento/src/reordenar/Sort.java
@@ -0,0 +1,72 @@
+package reordenar;
+
+public class Sort {
+
+	public static int[] bubble(int[] numArray) {
+
+		for (int j = numArray.length - 1; j >= 0; j--) {
+
+			int mayorIndex = 0;
+
+			for (int i = 1; i <= j; i++) {
+				if (numArray[i] >= numArray[mayorIndex]) {
+					mayorIndex = i;
+				}
+			}
+
+			numArray = Sort.substitute(numArray, mayorIndex, j);
+			// Sort.substitute(numArray, mayorIndex, j);
+		}
+
+		return numArray;
+	}
+
+	public static int[] quickSort(int[] numArray) {
+
+		int indiceInferior = 0;
+		int indiceSuperior = numArray.length - 1;
+
+		Sort.innerQuickSort(numArray, indiceInferior, indiceSuperior);
+
+		return numArray;
+	}
+
+	private static void innerQuickSort(int[] numArray, int indiceInferior, int indiceSuperior) {
+		int i = indiceInferior;
+		int j = indiceSuperior;
+
+		int pivote = numArray[indiceInferior + (int) ((indiceSuperior - indiceInferior) / 2)];
+
+		while (i <= j) {
+
+			while (numArray[i] < pivote) {
+				i++;
+			}
+			while (numArray[j] > pivote) {
+				j--;
+			}
+			if (i <= j) {
+				substitute(numArray, j, i);
+				i++;
+				j--;
+			}
+		}
+
+		if (indiceInferior < j) {
+			Sort.innerQuickSort(numArray, indiceInferior, j);
+		}
+		if (i < indiceSuperior) {
+			Sort.innerQuickSort(numArray, i, indiceSuperior);
+		}
+	}
+
+	private static int[] substitute(int[] array, int firstIndex, int secondIndex) {
+
+		int temp = array[firstIndex];
+		array[firstIndex] = array[secondIndex];
+		array[secondIndex] = temp;
+
+		return array;
+	}
+
+}
