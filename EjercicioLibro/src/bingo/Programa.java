9603c901936c8a00eed83a878a95163853fddbe1
inigo001 <ivique001@gmail.com>
2017-06-02 10:40:10 +0200

    y esto?

--- EjercicioLibro/src/bingo/Programa.java
+++ EjercicioLibro/src/bingo/Programa.java
@@ -4,13 +4,21 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Bingo bingo = new Bingo(50, 12);
-		//System.out.println(bingo.girar());
-		
-		BingoAmericano bingoAmericano = new BingoAmericano();		
-		bingoAmericano.crearCarton();	
-		
+
+		// Bingo bingo = new Bingo(50, 12);
+		// System.out.println(bingo.girar());
+
+		BingoAmericano bingoAmericano = new BingoAmericano();
+
+		bingoAmericano.crearCartones(15);
+
+		do {
+			bingoAmericano.girar();
+		} while (!bingoAmericano.getFinalizado());
+
+		System.out.println(bingoAmericano.getBolasExtraidas());
+		System.out.println(bingoAmericano.getCartonesBingo());
+
 	}
 
 }
