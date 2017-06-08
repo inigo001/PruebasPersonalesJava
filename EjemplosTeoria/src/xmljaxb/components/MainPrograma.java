0329c26759f76d01e919a882e8693a103849fcf2
inigo001 <ivique001@gmail.com>
2017-06-08 12:05:19 +0200

    :wine_glass: Funtziona

--- EjemplosTeoria/src/xmljaxb/components/MainPrograma.java
+++ EjemplosTeoria/src/xmljaxb/components/MainPrograma.java
@@ -1,26 +1,49 @@
 package xmljaxb.components;
 
-import xmljaxb.classes.*;
+import xmljaxb.classes.*; // Clases creadas por XJC
+
+import java.util.ArrayList; // Maravilloso ArrayList
+
+// Cosas para los XML.
 import javax.xml.bind.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 public class MainPrograma {
 
-	
 	// TODO Por hacer
 	public static void main(String[] args) {
 
 		try {
+
+			// Creamos el contexto Jax de la clase que queremos. En este caso
+			// sería conveniente que fuera la clase que agrupa la mayoría de del
+			// contenido del XML.
 			final JAXBContext jc = JAXBContext.newInstance(Rows.class);
+			// No se que es un unmarshaller, pero ya lo veremos.
 			final Unmarshaller u = jc.createUnmarshaller();
 
-			System.out.println(u);
+			// El Stream de datos. Cada sistema utiliza sus propias locuras para
+			// marcar la ruta, así que éste es el que, de todos, más sencillo
+			// resulta.
+			Source source = new StreamSource("data/desfibriladoreak.xml");
 
-			final JAXBElement<Rows> menuElement = (JAXBElement<Rows>) u
-					.unmarshal(MainPrograma.class.getResourceAsStream("./data/desfibriladoreak.xml"));
+			// Creamos un elemento de tipo JaxBElement con la clase que nos a
+			// creado XJC. Unmarshaleamos ( ¿(*.*)? ) los datos del XML teniendo
+			// en cuenta la clase que estamos usando.
+			JAXBElement<Rows> menuElement = u.unmarshal(source, Rows.class);
 
-			System.out.println("Nombre: " + menuElement.getValue().getNum());
+			// Creamos una lista con los elementos internos. Esta lista se
+			// compone de objetos de clase Row, creada con XJC y con métodos
+			// bastante sencillos de aprender a utilizar.
+			ArrayList<Row> listRow = (ArrayList<Row>) menuElement.getValue().getRow();
+
+			// Podemos acceder a los elementos del objeto usando los métodos que
+			// el XJC nos ha creado anteriormente.
+			System.out.println(listRow.get(8).getErakunde());
+
 		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 
